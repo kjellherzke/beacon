bc0b30dbc63f6811393cf9b342aa06251a1c9c70
kjellherzke <kjell.herzke@gmail.com>
2024-05-25 23:00:51 +0200

    fix: logo path, logo margin

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <div className="pt-12 px-16">
       <div className="flex items-center justify-between mb-12">
         <div className="flex items-center space-x-2">
-          <img src="/logo-512x512.png" className="w-8 h-8" />
-          <h1 className="text-secondary font-bold text-2xl">beacon</h1>
+          <img src="/beacon/logo-512x512.png" className="w-8 h-8" />
+          <h1 className="text-secondary font-bold text-2xl m-0 p-0">beacon</h1>
         </div>
       </div>
       <p className="text-xl mb-8 font-bold">Hello, traveller!</p>
