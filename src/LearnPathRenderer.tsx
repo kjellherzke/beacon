a01d37b95af75d16e2f42fa9a73bf97f59ef117e
kjellherzke <kjell.herzke@gmail.com>
2024-05-25 07:40:24 +0200

    fix: on-load nodes manipulation

--- src/LearnPathRenderer.tsx
+++ src/LearnPathRenderer.tsx
@@ -10,7 +10,7 @@ const nodeWidth = (str: string) => 17 + str.length * 10;
 
 interface Path {
   title: string;
-  nodes: PathNode[];
+  nodes?: PathNode[];
 }
 
 interface PathNode {
@@ -35,7 +35,7 @@ const generationColor = (generation: number) =>
 function SingleNode({ data }: { data: PathNode }) {
   return (
     <div
-      className="py-2 px-4 border-2 rounded-2xl bg-background hover:scale-105 hover:cursor-pointer transition-all"
+      className="py-2 px-4 border-2 rounded-2xl bg-background hover:scale-105 hover:cursor-pointer transition-all whitespace-nowrap"
       style={{
         position: "absolute",
         left: data.x,
@@ -63,23 +63,17 @@ function NodeLine({ data }: { data: PathNode }) {
     >
       {useMemo(
         () =>
-          data.nodes
-            ?.map((node) => ({
-              ...node,
-              height: nodeHeight,
-              width: nodeWidth(node.name),
-            }))
-            .map((node, i) => (
-              <line
-                key={i}
-                x1={data.x + data.width / 2}
-                y1={data.y + data.height / 2}
-                x2={node.x + node.width / 2}
-                y2={node.y + node.height / 2}
-                strokeWidth={4}
-                stroke={generationColor(data.generation)}
-              />
-            )),
+          data.nodes?.map((node, i) => (
+            <line
+              key={i}
+              x1={data.x + data.width / 2}
+              y1={data.y + data.height / 2}
+              x2={node.x + node.width / 2}
+              y2={node.y + node.height / 2}
+              strokeWidth={4}
+              stroke={generationColor(data.generation)}
+            />
+          )),
         [],
       )}
     </svg>
@@ -93,17 +87,11 @@ function Node({ data }: { data: PathNode }) {
       <div>
         {useMemo(
           () =>
-            data.nodes
-              ?.map((node) => ({
-                ...node,
-                height: nodeHeight,
-                width: nodeWidth(node.name),
-              }))
-              .map((node, i) => (
-                <div key={i}>
-                  <Node data={node} />
-                </div>
-              )),
+            data.nodes?.map((node, i) => (
+              <div key={i}>
+                <Node data={node} />
+              </div>
+            )),
           [],
         )}
         <NodeLine data={data} />
@@ -117,50 +105,65 @@ export default function LearnPathRenderer() {
 
   // WORKING CODE FOR GETTING ALL OF DIRECTORY
   useEffect(() => {
+    const recursiveChangeNodes = (
+      nodes: PathNode[],
+      generation: number,
+    ): PathNode[] | undefined =>
+      nodes?.map((n) => ({
+        ...n,
+        generation,
+        height: nodeHeight,
+        width: nodeWidth(n.name),
+        nodes: n.nodes
+          ? recursiveChangeNodes(n.nodes, generation + 1)
+          : undefined,
+      }));
+
+    const manipulatePath = (path: Path | null): Path | null => {
+      if (!path) return null;
+      return path.nodes
+        ? { ...path, nodes: recursiveChangeNodes(path.nodes, 0) }
+        : path;
+    };
+
     const modules = import.meta.glob(`/public/static/learningpath/*.json`);
     for (const path in modules) {
-      modules[path]().then((mod) => setPath(mod as Path));
+      modules[path]().then((mod) => setPath(manipulatePath(mod as Path)));
     }
   }, []);
 
   return (
-    <div className="h-[30rem] overflow-scroll border-2 border-secondary border-opacity-20 rounded-2xl p-5 select-none">
-      <div className="relative w-full h-[30rem]">
+    <div className="h-[30rem] w-[50rem] overflow-scroll border-2 border-secondary border-opacity-20 rounded-2xl p-5 select-none">
+      <div className="relative w-full h-full">
         {useMemo(
           () =>
-            path?.nodes
-              .map((node) => ({
-                ...node,
-                height: nodeHeight,
-                width: nodeWidth(node.name),
-              }))
-              .map((node, i, elements) => (
-                <>
-                  <Node key={i} data={node} />
-                  {elements.length - 1 > i && (
-                    <svg
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        top: 0,
-                        left: 0,
-                        position: "absolute",
-                        zIndex: -1,
-                      }}
-                    >
-                      <line
-                        key={i}
-                        x1={node.x + node.width / 2}
-                        y1={node.y + node.height / 2}
-                        x2={elements[i + 1].x + elements[i + 1].width / 2}
-                        y2={elements[i + 1].y + elements[i + 1].height / 2}
-                        strokeWidth={4}
-                        stroke={generationColor(node.generation)}
-                      />
-                    </svg>
-                  )}
-                </>
-              )),
+            path?.nodes?.map((node, i, elements) => (
+              <div key={i}>
+                <Node key={i} data={node} />
+                {elements.length - 1 > i && (
+                  <svg
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      top: 0,
+                      left: 0,
+                      position: "absolute",
+                      zIndex: -1,
+                    }}
+                  >
+                    <line
+                      key={i}
+                      x1={node.x + node.width / 2}
+                      y1={node.y + node.height / 2}
+                      x2={elements[i + 1].x + elements[i + 1].width / 2}
+                      y2={elements[i + 1].y + elements[i + 1].height / 2}
+                      strokeWidth={4}
+                      stroke={generationColor(node.generation)}
+                    />
+                  </svg>
+                )}
+              </div>
+            )),
           [path],
         )}
       </div>
