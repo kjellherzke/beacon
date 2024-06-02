59e3d59780f6e3cf3fff8c61a0a3e7d7badbdde5
kjellherzke <kjell.herzke@gmail.com>
2024-06-02 12:47:44 +0200

    feat: BREAKING new visuals; rm: unnecessary content;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       sans: "Space Grotesk",
     },
-
     extend: {
       colors: {
         primary: "#FFFFFF",
         secondary: "#A8A8A8",
         slate: "#303030",
         background: "#111111",
+        visualGraphs: {
+          generation0: "#f9f871",
+          generation1: "#ffc75f",
+          generation2: "#ff9671",
+          generation3: "#ff6f91",
+          generation4: "#d65db1",
+          generation5: "#845ec2",
+        },
       },
     },
   },
-  plugins: [],
-};
+} satisfies Config;
